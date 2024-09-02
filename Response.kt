8b5d11c04d7cbbae595d25f6cc499d6fa00522b2
gabriel-classique <dbmm@tuta.io>
2024-09-02 16:37:44 +0200

    Add files via upload

---
+++ Response.kt
@@ -0,0 +1,39 @@
+/*-----------------------------------------------------------------------------------------------------*/
+
+typealias RootError = Error
+
+sealed interface Response<out D, out E: RootError> {
+    data class Success<out D, out E: RootError> (val data: D): Response<D, E>
+    data class Failure<out D, out E: RootError> (val error: E): Response<D, E>
+}
+
+/*-----------------------------------------------------------------------------------------------------*/
+
+sealed interface Error
+
+enum class ApiError: Error {
+    QUERY_TOO_SHORT,
+    NOT_FOUND,
+    CONNECTION_ERROR,
+}
+
+
+/*--------------------------------  USAGE  ---------------------------------------------------------------------*/
+
+// fun get(query): Response<List<ApiDTO>, ApiError>
+
+val apiResult = repository.get(query)
+
+when(apiResult){
+    is Response.Success -> { 
+	// do something with apiResult.data
+    }
+    
+    is Response.Failure -> {
+        when(apiResult.error){
+            QUERY_TOO_SHORT ->
+            NOT_FOUND ->
+            CONNECTION_ERROR ->
+	}
+    }
+}
